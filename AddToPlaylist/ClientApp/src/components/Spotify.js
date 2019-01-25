29564e27bb089f57e9bc713d39bf2f9c721a958d
maozh <maoz4h@gmail.com>
2019-01-25 18:03:32 +0200

    First push

---
+++ AddToPlaylist/ClientApp/src/components/Spotify.js
@@ -0,0 +1,270 @@
+﻿import React, { Component } from 'react';
+
+export class Spotify extends Component {
+    displayName = Spotify.name
+
+    constructor(props) {
+        super(props);
+        var authorizationCode = this.getStorage('authorizationCode');
+        if (this.state) {
+            if (!this.state.authorizationCode) {
+                if (!authorizationCode) {
+                    authorizationCode = this.getQuery("code");
+                }
+                this.setState(() => ({
+                    authorizationCode: authorizationCode,
+                }));
+                this.setStorage('authorizationCode', this.state.authorizationCode);
+            }
+        }
+        else {
+            this.state = {
+                loggedIn: false,
+                header: 'Logged Out'
+            }
+
+            authorizationCode = (authorizationCode !== "" ? authorizationCode : undefined) || this.getQuery("code");
+            if (authorizationCode && authorizationCode !== "") {
+                this.state.authorizationCode = authorizationCode;
+                this.setStorage('authorizationCode', '');
+                this.state.loggedIn = true;
+            }
+
+            if (this.state.authorizationCode && !this.state.clientId) {
+                this.state.clientId = this.getStorage('clientId');
+                this.setStorage('clientId', '');
+                if (this.state.clientId) this.state.header = 'Loading';
+            }
+        }
+
+        if (this.state.loggedIn && !this.state.token) {
+            this.getAccessToken();
+        }
+        else if (this.state.loggedIn && this.state.token) {
+
+        }
+    }
+
+    getStorage(property) {
+        let storageProperty = localStorage.getItem(property);
+        return (storageProperty === "undefined" || storageProperty === "") ? undefined : storageProperty;
+    }
+
+    setStorage(propertyString, propertyValue) {
+        localStorage.setItem(propertyString, propertyValue);
+    }
+
+    getCurrentUserId() {
+        let me = this;
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = (e) => {
+            if (request.readyState !== 4) {
+                return;
+            }
+
+            if (request.status === 200) {
+                let user = JSON.parse(request.response);
+                let userId = user.id;
+                me.setState(() => ({
+                    userId: userId
+                }));
+            } else {
+                console.warn('error');
+            }
+        };
+
+        request.open('GET', 'https://api.spotify.com/v1/me');
+        request.setRequestHeader('Authorization', "Bearer " + me.state.token);
+        request.send();
+    }
+
+    getUserPlaylists() {
+        let me = this;
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = (e) => {
+            if (request.readyState !== 4) {
+                return;
+            }
+
+            if (request.status === 200) {
+                let user = JSON.parse(request.response);
+                let userId = user.id;
+                me.setState(() => ({
+                    userId: userId
+                }));
+                request = new XMLHttpRequest();
+                request.onreadystatechange = (e) => {
+                    if (request.readyState !== 4) {
+                        return;
+                    }
+
+                    if (request.status === 200) {
+                        let playlistsArr = JSON.parse(request.response);
+                        me.setState(() => ({
+                            userPlaylists: playlistsArr
+                        }));
+                        //playlistsArr.items.findIndex((i) => i.name === this.state.playlistName) > 0
+                    } else {
+                        console.warn('error');
+                    }
+                };
+
+                request.open('GET', 'https://api.spotify.com/v1/users/' + me.state.userId + '/playlists');
+                request.setRequestHeader('Authorization', "Bearer " + me.state.token);
+                request.send();
+            } else {
+                console.warn('error');
+            }
+        };
+
+        request.open('GET', 'https://api.spotify.com/v1/me');
+        request.setRequestHeader('Authorization', "Bearer " + me.state.token);
+        request.send();
+    }
+
+    handleLogin(e) {
+        e.preventDefault();
+        let me = this;
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = (e) => {
+            if (request.readyState !== 4) {
+                return;
+            }
+
+            if (request.status === 200) {
+                let response = JSON.parse(request.response);
+                response = JSON.parse(response);
+                me.setState(() => ({
+                    clientId: response.client_id,
+                    //clientSecret: response.client_secret
+                }));
+                me.setStorage('clientId', me.state.clientId);
+                //me.setStorage('clientSecret', me.state.clientSecret);
+                let url = 'https://accounts.spotify.com/authorize';
+                url += '?response_type=code';
+                url += '&client_id=' + encodeURIComponent(me.state.clientId);
+                url += '&scope=' + encodeURIComponent("user-read-private user-modify-private playlist-modify-private");
+                //url += '&show_dialog=true';
+                url += '&redirect_uri=' + "https://localhost:44313/spotify";
+                window.location = url;
+            } else {
+                console.warn('error');
+            }
+        };
+
+        request.open('GET', 'api/Spotify/Login');
+        request.setRequestHeader('Accept', 'application/json');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send();
+    }
+
+    getQuery(q) {
+        return (window.location.search.match(new RegExp('[?&]' + q + '=([^&]+)')) || [, undefined])[1];
+    }
+
+    getAccessToken() {
+        let me = this;
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = (e) => {
+            if (request.readyState !== 4) {
+                return;
+            }
+
+            if (request.status === 200) {
+                let response = JSON.parse(request.response);
+                let date = new Date();
+                me.setState(() => ({
+                    token: response.access_token,
+                    header: 'Logged In',
+                    expiresIn: date.setSeconds(date.getSeconds() + response.expires_in - 1),
+                    tokenType: response.token_type,
+                    scope: response.scope,
+                    refreshToken: response.refresh_token,
+                }));
+                me.getCurrentUserId();
+                me.getUserPlaylists();
+            } else {
+                console.warn('error');
+            }
+        };
+
+        request.open('GET', 'api/Spotify/GetToken?' + 'auth=' + this.state.authorizationCode);
+        request.send();
+    }
+
+    readSingleFile(file) {
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                this.state.content = e.target.result;
+                this.handleAddToPlaylist();
+            }.bind(this);
+            reader.readAsText(file);
+        } else {
+            alert("Failed to load file");
+        }
+        document.getElementById('fileinput').addEventListener('change', this.readSingleFile, false);
+    }
+
+
+    isPlaylistExist(playlistName) {
+        this.getUserPlaylists();
+        debugger;
+    }
+
+    handleAddToPlaylist(e) {
+        if (!this.state.playlistName) this.state.playlistName = e.target.elements.playlistName.value.trim();
+        if (!this.state.playlistName) this.state.delimiter = e.target.elements.delimiter.value.trim();
+
+        if (this.isPlaylistExist(this.state.playlistName)) {
+
+        }
+        else {
+            this.createPlaylist(this.state.playlistName);
+        }
+        if (this.state.content) {
+            const content = this.state.content;
+            this.setState(() => ({
+                content: undefined
+            }));
+            const titleArtistsPairs = this.getPairsFromContent(content, this.state.delimiter);
+
+        }
+        else {
+            e.preventDefault();
+            const file = e.target.elements.fileinput.files[0];
+            this.readSingleFile(file);
+        }
+    }
+
+    getPairsFromContent(content, delimiter) {
+        let pairs = [];
+        let lines = content.split("\n");
+        pairs = (lines.map((line) => (
+            line.split(delimiter)
+        )));
+        return pairs;
+    }
+
+    render() {
+        return (
+            <div>
+                <p id="header">{this.state.header}</p>
+                {!this.state.loggedIn &&
+                    <form onSubmit={this.handleLogin.bind(this)}>
+                        <button>Login</button>
+                    </form>
+                }
+                {this.state.token &&
+                    <form onSubmit={this.handleAddToPlaylist.bind(this)}>
+                        Playlist name<input type="text" name="playlistName" />
+                        <br />
+                        Delimiter<input type="text" name="delimiter" />
+                        <input type="file" id="fileinput" name="fileinput" />
+                        <button>Add Songs to Playlist</button>
+                    </form>
+                }
+            </div>
+        );
+    }
+}
---
+++ AddToPlaylist/Controllers/SpotifyController.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ReactDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class SpotifyController : Controller
+    {
+        private String clientId = "4836c599ff1f481b972243e3abd00541";
+        private String clientSecret = "37ef8d95df4947e7b95f0391fcd5589c";
+
+        [HttpGet("[action]")]
+        public string Login([FromBody]LoginInput input)
+        {
+            Dictionary<string, string> res = new Dictionary<string, string>();
+            res.Add("client_id", "4836c599ff1f481b972243e3abd00541");
+            res.Add("client_secret", "37ef8d95df4947e7b95f0391fcd5589c");
+            return JsonConvert.SerializeObject(res);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<object> GetToken()
+        {
+            Dictionary<string, string> res = new Dictionary<string, string>();
+            res.Add("token", "4836c599ff1f481b972243e3abd00541");
+            String response;
+            var query = "?grant_type=authorization_code" +
+            "&code=" + Request.QueryString.Value.Split('=')[1] +
+            "&redirect_uri=https://localhost:44313/spotify";
+
+
+            StringContent stringContent = new StringContent("", UnicodeEncoding.UTF8, "application/x-www-form-urlencoded");
+            //stringContent.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://accounts.spotify.com/api/token");
+                //client.DefaultRequestHeaders.Add("Content-Type", "application/x-www-form-urlencoded");
+                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));// = "application/x-www-form-urlencoded";//.Add("Content-Type", "application/x-www-form-urlencoded");
+                //client.DefaultRequestHeaders.AddWithoutValidation("Content-Type", "application/x-www-form-urlencoded");
+                client.DefaultRequestHeaders.Add("Authorization", "Basic " + encoding(clientId + ':' + clientSecret));
+                var result = await client.PostAsync(query, stringContent);
+                response = await result.Content.ReadAsStringAsync();
+            }
+            return JsonConvert.DeserializeObject(response);
+            //return response;
+        }
+
+        public void AddContentTypeHeader(HttpRequestMessage message, string value)
+        {
+            message.Content = new StringContent("");
+            message.Content.Headers.ContentType = new MediaTypeHeaderValue(value);
+        }
+
+        /*
+        request.open('POST', 'https://accounts.spotify.com/api/token');
+        request.setRequestHeader('Authorization', 'Basic ' + btoa(me.state.clientId + ':' + me.state.clientSecret));
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        var query = 'grant_type=authorization_code' +
+            '&code=' + me.state.authorizationCode +
+            '&redirect_uri=https://localhost:44313/spotify';
+        request.send(query);
+        */
+        public class LoginInput
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+
+        public string encoding(string toEncode)
+        {
+            byte[] bytes = Encoding.GetEncoding(28591).GetBytes(toEncode);
+            string toReturn = System.Convert.ToBase64String(bytes);
+            return toReturn;
+        }
+    }
+}
+
+/*
+string page = "https://accounts.spotify.com/authorize";
+var parameters = new Dictionary<string, string>();
+parameters.Add("client_id", "4836c599ff1f481b972243e3abd00541");
+parameters.Add("response_type", "code");
+parameters.Add("redirect_uri", "");
+var query = HttpUtility.ParseQueryString(string.Empty);
+query["client_id"] = "4836c599ff1f481b972243e3abd00541";
+query["response_type"] = "code";
+query["redirect_uri"] = "https://localhost:44313/spotify";
+query["scope"] = "user-read-private user-modify-private playlist-modify-private";
+string queryString = query.ToString();
+string responseBody = String.Empty;
+// ... Use HttpClient.
+using (HttpClient client = new HttpClient())
+{
+    // Call asynchronous network methods in a try/catch block to handle exceptions
+    try
+    {
+        HttpResponseMessage response = await client.GetAsync("https://accounts.spotify.com/authorize?" + queryString);
+        response.EnsureSuccessStatusCode();
+        responseBody = await response.Content.ReadAsStringAsync();
+        // Above three lines can be replaced with new helper method below
+        // string responseBody = await client.GetStringAsync(uri);
+
+        Console.WriteLine(responseBody);
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine("\nException Caught!");
+        Console.WriteLine("Message :{0} ", e.Message);
+    }
+}*/
+
+//string url = "https://accounts.spotify.com/authorize";
+//var parameters = new Dictionary<string, string>();
+//parameters.Add("client_id", "4836c599ff1f481b972243e3abd00541");
+//parameters.Add("response_type", "code");
+//parameters.Add("redirect_uri", "");
+
+//var encodedContent = new FormUrlEncodedContent(parameters);
+//HttpClient client = new HttpClient();
+//var response = await client.GetAsync(url, encodedContent).ConfigureAwait(false);
+//if (response.StatusCode == HttpStatusCode.OK)
+//{
+//    // Do something with response. Example get content:
+//    // var responseContent = await response.Content.ReadAsStringAsync ().ConfigureAwait (false);
+//}
